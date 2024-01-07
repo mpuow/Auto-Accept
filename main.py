98b5e0c98bc95bb9b362571d796598bb400427ca
unknown <mpuow00@gmail.com>
2024-01-08 00:55:58 +1100

    updated

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from time import sleep
 # name of image we are searching for
 search_image = 'search.png'
 
-# endtime is how many 5 sec intervals you want. eg. 120 is 10 minutes - 12 per min
-
 # search duration in minutes, will cancel if time limit reached
 max_minutes = 0.5
 duration = max_minutes * 12     # 12 searches per minute (1 every 5 seconds)
